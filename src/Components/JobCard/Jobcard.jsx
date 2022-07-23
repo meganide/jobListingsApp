695637c8feaf395d479695f10adfd353e66b5e33
Meganide <renashassan@hotmail.com>
2022-07-24 00:57:10 +0200

    showing all icontexts now

--- src/Components/JobCard/Jobcard.jsx
+++ src/Components/JobCard/Jobcard.jsx
@@ -2,58 +2,65 @@ import { Card, CardContent } from "@mui/material";
 import React from "react";
 import "./jobcard.css";
 
-function Jobcard({jobs}, props) {
-
+function Jobcard({ jobs }, props) {
   function clickFilterItem(event) {
     console.log("i am clicked");
   }
 
-
   return (
     <>
-    {jobs.map((job) => {
-      return (
-      <Card
-      key = {job.id}
-      className={props.indexNumber < 2 && "card"}
-      sx={{ margin: { xs: "60px 0", md: "20px 0" }, overflow: "visible" }}
-    >
-      <CardContent className="cardcontent">
-        <div className="logoAndTextWrapper">
-          <img src={job.logo} alt="company logo" />
-          <div className="jobInfo">
-            <div className="titleInfo">
-              <p className="companyTitle">{job.company}</p>
-              {job.new && <p className="new">NEW!</p>}
-              {job.featured && <p className="featured">FEATURED</p>}
-            </div>
-            <p className="jobtitle">{job.position}</p>
-            <p className="shorts">
-              {job.postedAt} &nbsp; · &nbsp; {job.contract} &nbsp; · &nbsp;
-              {job.location}
-            </p>
-          </div>
-        </div>
-        <div className="icontexts">
-          {job.languages.length > 0 &&
-            job.languages.map((item, index) => {
-              const randomNumber = Math.random() * (100000 - 20) + 20;
-              return (
-                <p
-                  key={randomNumber}
-                  className="icon"
-                  onClick={clickFilterItem}
-                >
-                  {item}
-                </p>
-              );
-            })}
-        </div>
-      </CardContent>
-    </Card>
-      )
-      
-    }) }
+      {jobs.map((job) => {
+        const randomNumber = Math.random() * (100000 - 20) + 20;
+        let itemsArray = [job.role];
+        itemsArray.push(job.level);
+        itemsArray = itemsArray.concat(job.tools);
+        itemsArray = itemsArray.concat(job.languages);
+
+        itemsArray = itemsArray.filter((item) => {
+          return typeof item === "string";
+        });
+
+
+        return (
+          <Card
+            key={job.id}
+            className={props.indexNumber < 2 && "card"}
+            sx={{ margin: { xs: "60px 0", md: "20px 0" }, overflow: "visible" }}
+          >
+            <CardContent className="cardcontent">
+              <div className="logoAndTextWrapper">
+                <img src={job.logo} alt="company logo" />
+                <div className="jobInfo">
+                  <div className="titleInfo">
+                    <p className="companyTitle">{job.company}</p>
+                    {job.new && <p className="new">NEW!</p>}
+                    {job.featured && <p className="featured">FEATURED</p>}
+                  </div>
+                  <p className="jobtitle">{job.position}</p>
+                  <p className="shorts">
+                    {job.postedAt} &nbsp; · &nbsp; {job.contract} &nbsp; ·
+                    &nbsp;
+                    {job.location}
+                  </p>
+                </div>
+              </div>
+              {itemsArray.map((item) => {
+                return (
+                  <div className="icontexts">
+                    <p
+                      key={randomNumber}
+                      className="icon"
+                      onClick={clickFilterItem}
+                    >
+                      {item}
+                    </p>
+                  </div>
+                );
+              })}
+            </CardContent>
+          </Card>
+        );
+      })}
     </>
   );
 }
