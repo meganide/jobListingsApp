3136a447bc274dc507445d578fe879128cd49773
Meganide <renashassan@hotmail.com>
2022-07-23 23:54:24 +0200

    added useState for the data.js file in joblistings

--- src/Components/JobCard/Jobcard.jsx
+++ src/Components/JobCard/Jobcard.jsx
@@ -2,51 +2,40 @@ import { Card, CardContent } from "@mui/material";
 import React from "react";
 import "./jobcard.css";
 
-function Jobcard(props) {
-  let itemsArray = [props.role];
-  itemsArray.push(props.level);
-  itemsArray = itemsArray.concat(props.tools);
-  itemsArray = itemsArray.concat(props.languages);
-
-  itemsArray = itemsArray.filter((item) => {
-    return typeof item === "string";
-  });
-
-
-  function handleClickEvent(event) {
-    event.currentTarget.classList.toggle('activeBackground');
-    const whichClicked = event.target.innerText;
-  }
+function Jobcard({jobs}, props) {
 
   return (
-    <Card
+    <>
+    {jobs.map((job) => {
+      return (
+      <Card
+      key = {job.id}
       className={props.indexNumber < 2 && "card"}
       sx={{ margin: { xs: "60px 0", md: "20px 0" }, overflow: "visible" }}
     >
       <CardContent className="cardcontent">
         <div className="logoAndTextWrapper">
-          <img src={props.img} alt="company logo" />
+          <img src={job.logo} alt="company logo" />
           <div className="jobInfo">
             <div className="titleInfo">
-              <p className="companyTitle">{props.company}</p>
-              {props.itemNew && <p className="new">NEW!</p>}
-              {props.itemFeatured && <p className="featured">FEATURED</p>}
+              <p className="companyTitle">{job.company}</p>
+              {job.new && <p className="new">NEW!</p>}
+              {job.featured && <p className="featured">FEATURED</p>}
             </div>
-            <p className="jobtitle">{props.title}</p>
+            <p className="jobtitle">{job.position}</p>
             <p className="shorts">
-              {props.one} &nbsp; · &nbsp; {props.two} &nbsp; · &nbsp;
-              {props.three}
+              {job.postedAt} &nbsp; · &nbsp; {job.contract} &nbsp; · &nbsp;
+              {job.location}
             </p>
           </div>
         </div>
         <div className="icontexts">
-          {itemsArray.length > 0 &&
-            itemsArray.map((item, index) => {
+          {job.languages.length > 0 &&
+            job.languages.map((item, index) => {
               const randomNumber = Math.random() * (100000 - 20) + 20;
               return (
                 <p
                   key={randomNumber}
-                  onClick={handleClickEvent}
                   className="icon"
                 >
                   {item}
@@ -56,6 +45,10 @@ function Jobcard(props) {
         </div>
       </CardContent>
     </Card>
+      )
+      
+    }) }
+    </>
   );
 }
 export default Jobcard;
--- src/Components/Joblistings/Joblistings.jsx
+++ src/Components/Joblistings/Joblistings.jsx
@@ -5,39 +5,17 @@ import "./joblistings.css";
 import { data } from "../../data.js";
 import { useState } from "react";
 
-
-
 function Joblistings(props) {
-
+  // Manage the state of our data
+  const [jobs, setJobs] = useState(data);
 
   return (
     <div className="mainContainer">
       <Container>
-        {data && data.map((item, index) => {
-          return (
-            <Jobcard
-              key={item.id}
-              img={item.logo}
-              company={item.company}
-              title={item.position}
-              one={item.postedAt}
-              two={item.contract}
-              three={item.location}
-              itemNew={item.new}
-              itemFeatured={item.featured}
-              languages={item.languages}
-              tools={item.tools}
-              role={item.role}
-              level={item.level}
-              indexNumber={index}
-            />
-          );
-        })}
-        <Jobcard />
+        <Jobcard jobs={jobs}/>
       </Container>
     </div>
   );
-
 }
 
 export default Joblistings;
